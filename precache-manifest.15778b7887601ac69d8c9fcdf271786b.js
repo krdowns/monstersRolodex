c8680613ae989baca8f48add1599727ed3d9a22b
Kenny Downs <kdowns@repairpal.com>
2019-07-02 14:38:41 -0700

    Updates

---
+++ precache-manifest.15778b7887601ac69d8c9fcdf271786b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707cf96f3d5c438fa3fc6cd0d8861d60",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "5fe9fcedae53717582c3",
+    "url": "/monstersRolodex/static/css/main.ef4bb8f7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monstersRolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5fe9fcedae53717582c3",
+    "url": "/monstersRolodex/static/js/main.0a7c6181.chunk.js"
+  },
+  {
+    "revision": "adbf709dcf00b1922f72",
+    "url": "/monstersRolodex/static/js/runtime~main.15a48f79.js"
+  }
+]);
